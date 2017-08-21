3886f3c882274deca4d830968ae780a711c06b0b
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2017-08-21 15:47:53 +0200

    Rename: nonTerminalQuestionMark => opt.

---
+++ spec/opt.js
@@ -0,0 +1,59 @@
+describe("The question mark quantifier", function() {
+  
+  var f = new InterpreterMethodFactory();
+  var i;
+  
+  beforeEach(function() {
+    i = {};
+    i.a = f.atom(/a/);
+    i.b = f.atom(/b/);
+    i.ar = f.atom(/a/, function() {
+      return "the result";
+    });
+    
+  });
+  
+  it("matches its part once", function() {
+    i.maybeA = f.opt("a");
+    
+    expect(function() {
+      i.maybeA("a");
+    }).not.toThrow();
+    
+  });
+  
+ it("matches the empty string", function() {
+    i.maybeA = f.opt("a");
+    i.ab = f.group("maybeA", "b", function() {});
+    
+    expect(function() {
+      i.ab("b");
+    }).not.toThrow();
+    
+  });
+  
+  it("returns the result of its part", function() {
+    i.maybeA = f.opt("ar");
+    
+    expect(i.maybeA("a")).toBe("the result");
+  });
+  
+  it("returns the default value if it didn't match", function() {
+    i.maybeA = f.opt("ar", "the default value");
+    
+    expect(i.maybeA("")).toBe("the default value");
+  });
+  
+  it("calls its part as a method of the interpreter", function() {
+    i.charEater = f.atom(/./, function(char) {
+      this.theChar = char;
+    });
+    
+    i.opt = f.opt("charEater");
+    
+    i.opt("a");
+    
+    expect(i.theChar).toBe("a");
+  });
+  
+});
