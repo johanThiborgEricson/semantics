410373b6e562f9cf0a8d1a95ffaee90c6faadc21
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2017-09-15 16:18:13 +0200

    Semantics! should tell if it is instructed to use a method that isn't there.

---
+++ spec/error.js
@@ -0,0 +1,22 @@
+describe("An error", function() {
+  
+  var interpreter;
+  var f = new InterpreterMethodFactory();
+  
+  beforeEach(function() {
+    interpreter = {
+      
+    };
+    
+  });
+  
+  it("is thrown if the interpreter doesn't have a method with the name", 
+  function() {
+    interpreter.program = f.group("nonexistent");
+    
+    expect(function() {
+      interpreter.program("");
+    }).toThrowError("nonexistent is not a method of the interpreter");
+  });
+  
+});
