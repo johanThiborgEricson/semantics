29bc6d92f9a59bb916e35bc81b1c24c27ba47424
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2017-09-21 15:53:03 +0200

    Parsing the code of the code pointer fails at trailing comma in object literal.

--- spec/inception/inception.js
+++ spec/inception/inception.js
@@ -1,11 +1,35 @@
-describe("An XMLHttpRequest", function() {
+describe("The source code of CodePointer.js", function() {
+  
+  var CodePointerBackup;
+  var codePointerSourceCode;
+  var interpreter = new JavaScriptInterpreter();
   
   beforeEach(function(done) {
+    CodePointerBackup = window.CodePointer;
+    function reqListener () {
+      codePointerSourceCode = this.responseText;
+      done();
+    }
     
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", reqListener);
+    oReq.open("GET", "src/CodePointer.js");
+    oReq.send();
   });
   
-  it("can read retrieve the source code of CodePointer.js", function() {
-    expect(window.codePointerSourceCode).toBeDefined();
+  afterEach(function() {
+    window.CodePointer = CodePointerBackup;
+    delete window.codePointerSourceCode;
+  });
+  
+  it("can be loaded by an XMLHttpRequest", function() {
+    expect(codePointerSourceCode).toBeDefined();
+  });
+  
+  xit("can be run by the JavaScriptInterpreter", function() {
+    expect(function() {
+      interpreter.program(codePointerSourceCode);
+    }).not.toThrow();
   });
   
 });
