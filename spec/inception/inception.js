4c4c6095eb8bc5ba3d7e6b66f95dee83cae4de01
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2017-09-21 15:16:51 +0200

    Inception test stub.

---
+++ spec/inception/inception.js
@@ -0,0 +1,11 @@
+describe("An XMLHttpRequest", function() {
+  
+  beforeEach(function(done) {
+    
+  });
+  
+  it("can read retrieve the source code of CodePointer.js", function() {
+    expect(window.codePointerSourceCode).toBeDefined();
+  });
+  
+});
