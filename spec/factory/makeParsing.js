727c813afba21648c01d8803797e34a62d99aeb2
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2017-10-01 18:27:00 +0200

    The make parsing method can be interpreted.

--- spec/factory/makeParsing.js
+++ spec/factory/makeParsing.js
@@ -1,6 +1,10 @@
 describe("The make parsing method transforms a regex so that it", function() {
   
-  var f = new InterpreterMethodFactory();
+  var f;
+  
+  beforeAll(function() {
+    f = new InterpreterMethodFactory();
+  });
   
   it("matches the beginning of a string", function() {
     expect(f.makeParsing(/a/).exec("a")[0]).toBe("a");
